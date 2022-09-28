605be31d8889808bf08ae04dcf10587ed1c519fa
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-09-29 06:53:36 +0700

    add book model

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model{
+    protected $table = 'book';
+
+    protected $fillable = [
+        'name',
+        'price',
+        'desc',
+        'status'
+    ];
+}
