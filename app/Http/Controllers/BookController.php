bdfa9fe5555d5e34854c2e5d816dff7b7c7d1828
Adrian Metanoia Gawa <tfadriangawa@gmail.com>
2022-09-29 07:35:17 +0700

    add rules for create book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,6 +6,12 @@ use App\Models\Book;
 
 class BookController extends Controller {
     public function create(Request $request) {
+        $this->validate($request, [
+            "name" => "required|unique:book",
+            "price" => "required",
+            "status" => "required"
+        ]);
+
         $data = $request->all();
         $book = Book::create($data);
 
